30aecd758975b31953ccd848cacb0c7c452323c3
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-10-13 20:14:26 +0900

    update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def parse_args():
     parser.add_argument('--csv_path', type=str, default="C:/Users/kbh/Desktop/CNI/test/updated_subject_paths.csv", help='Path to the CSV file containing DICOM paths and labels')
     parser.add_argument('--train_base_dir', type=str, default="/home/kbh/Downloads/nii", help='Base directory for training DICOM files')
     parser.add_argument('--modality', type=str, default="FLAIR", help='Data modality')
-    parser.add_argument('--batch_size', type=int, default=1 , help='Batch size for DataLoaders')
+    parser.add_argument('--batch_size', type=int, default=16 , help='Batch size for DataLoaders')
     parser.add_argument('--epochs', type=int, default=50, help='Number of training epochs')
 
     parser.add_argument('--image_size', type=int,
